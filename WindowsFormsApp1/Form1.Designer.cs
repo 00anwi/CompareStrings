408ba527a45641ff310249184c0e540327f624ef
Andreas <andreas_s_w@hotmail.com>
2019-04-09 22:26:23 +0200

    First

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtbString1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbString2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblDiff = new System.Windows.Forms.Label();
+            this.lblDiffFault = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(304, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Compare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtbString1
+            // 
+            this.txtbString1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtbString1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbString1.Location = new System.Drawing.Point(12, 147);
+            this.txtbString1.Name = "txtbString1";
+            this.txtbString1.Size = new System.Drawing.Size(773, 41);
+            this.txtbString1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "String One";
+            // 
+            // txtbString2
+            // 
+            this.txtbString2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtbString2.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbString2.Location = new System.Drawing.Point(12, 228);
+            this.txtbString2.Name = "txtbString2";
+            this.txtbString2.Size = new System.Drawing.Size(773, 41);
+            this.txtbString2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "String Two";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(536, 47);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Compare strings for differences";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 349);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 33);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Difference:";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(384, 33);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Difference with fault calculation:";
+            // 
+            // lblDiff
+            // 
+            this.lblDiff.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDiff.AutoSize = true;
+            this.lblDiff.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiff.Location = new System.Drawing.Point(157, 349);
+            this.lblDiff.Name = "lblDiff";
+            this.lblDiff.Size = new System.Drawing.Size(101, 33);
+            this.lblDiff.TabIndex = 8;
+            this.lblDiff.Text = "123456";
+            // 
+            // lblDiffFault
+            // 
+            this.lblDiffFault.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDiffFault.AutoSize = true;
+            this.lblDiffFault.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiffFault.Location = new System.Drawing.Point(396, 398);
+            this.lblDiffFault.Name = "lblDiffFault";
+            this.lblDiffFault.Size = new System.Drawing.Size(71, 33);
+            this.lblDiffFault.TabIndex = 9;
+            this.lblDiffFault.Text = "1234";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 461);
+            this.Controls.Add(this.lblDiffFault);
+            this.Controls.Add(this.lblDiff);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbString2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbString1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtbString1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbString2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblDiff;
+        private System.Windows.Forms.Label lblDiffFault;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        /// <summary>
+        /// Checks the difference between two strings
+        /// </summary>
+        /// <returns>Number of Differnces</returns>
+        private int CalculateDifferenceStrings(string String1, string String2)
+        {
+            ArrayList list = new ArrayList();
+            ArrayList list2 = new ArrayList();
+            bool StringsDiffLenght = false;
+            int Result = 0;
+
+            try
+            {
+                //Adds the index of all different characters between String2 and String1 to list
+                for (int i = 0; i < String1.Length; ++i)
+                {
+                    if (i >= String2.Length)
+                        list.Add(i);
+                    else if (String1[i] != String2[i])
+                        list.Add(i);
+                }
+
+                lblDiff.Text = list.Count.ToString();
+
+                //Checks if String2 data is longer then String1
+                //This is incase String2 have one or more characters somewhere in the string. 
+                //Example 
+                //String1=1234 and String=11234
+                //We know from list that index 1, 2, 3 is wrong
+                //But if we remove the string difference from the first wrong index=1
+                //We get String2=1234 and no differences
+                if (String2.Length > String1.Length)
+                {
+                    int[] indicesCheck = (int[])list.ToArray(typeof(int));
+                    int StringLenghtDifference = String2.Length - String1.Length;  //Gets lenght differnce
+                    string NewString2 = "";
+                    if (list.Count == 0)
+                    {
+                        NewString2 = String2.Remove(String1.Length, StringLenghtDifference); //Removes characters from index of first fault and the character lenght difference between String1 and String2
+                    }
+                    else
+                    {
+                        NewString2 = String2.Remove(indicesCheck[0], StringLenghtDifference); //Removes characters from index of first fault and the character lenght difference between String1 and String2
+                    }
+
+                    //Adds the index of all different characters between NewString2 and String1 to list2
+                    for (int i = 0; i < String1.Length; ++i)
+                    {
+                        if (i >= NewString2.Length)
+                            list2.Add(i);
+                        else if (String1[i] != NewString2[i])
+                            list2.Add(i);
+                    }
+
+                    //This is for when string is different but not different lengths other wise it would send back 0 even if all characters are different
+                    StringsDiffLenght = true;
+                }
+                else if (String1.Length > String2.Length)
+                {
+                    int[] indicesCheck = (int[])list.ToArray(typeof(int));
+                    int StringLenghtDifference = String1.Length - String2.Length;  //Gets lenght differnce
+                    string NewString2 = "";
+
+                    NewString2 = ReturnXNbrOfQuestionMark(StringLenghtDifference) + String2;
+
+                    //Adds the index of all different characters between NewString2 and String1 to list2
+                    for (int i = 0; i < String1.Length; ++i)
+                    {
+                        if (i >= NewString2.Length)
+                            list2.Add(i);
+                        else if (String1[i] != NewString2[i])
+                            list2.Add(i);
+                    }
+
+                    //This is for when string is different but not different lengths other wise it would send back 0 even if all characters are different
+                    StringsDiffLenght = true;
+                }
+
+
+                int[] indices = (int[])list.ToArray(typeof(int));
+                int[] indices2 = (int[])list2.ToArray(typeof(int));
+
+                if (indices2.Count() < indices.Count() && StringsDiffLenght)
+                {
+                    //Counts how many indexed faults in list2 if character lenght is different and list2 countains less indexed faults
+                    foreach (var indexes2 in list2)
+                    {
+                        ++Result;
+                    }
+                }
+                else
+                {
+                    //Counts how many indexed faults in list
+                    foreach (var indexes in list)
+                    {
+                        ++Result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return String1.Length;
+            }
+
+            return Result;
+        }
+
+        private string ReturnXNbrOfQuestionMark(int X)
+        {
+            string ReturnString = "";
+            for (int i = 0; i < X; i++)
+            {
+                ReturnString += "?";
+            }
+
+            return ReturnString;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str1 = txtbString1.Text;
+            string str2 = txtbString2.Text;
+
+            lblDiffFault.Text = CalculateDifferenceStrings(str1, str2).ToString();
+        }
+    }
+}
